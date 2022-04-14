c8decb3cb85cf780b9e02c73558fe65da596e022
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-14 18:48:07 +0200

    Correct linters errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './Redux/ConfigureStore';
-import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
