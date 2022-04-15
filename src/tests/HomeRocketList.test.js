ced3e2909df57d496970823101c544ff8e1df7a8
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-15 11:47:28 +0200

    Add test for home rocket file component

---
+++ src/tests/HomeRocketList.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../Redux/configureStore';
+import RocketList from '../components/HomeRocket/HomeRocketList';
+
+describe('List oof Rocket test ', () => {
+  it('Should renders the Rocket page succefully', () => {
+    const app = renderer.create(
+      <Provider store={store}>
+        <RocketList />
+      </Provider>,
+    ).toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
